e236f844bc0b39ac5b0b81c1eeac5c88a1802d69
dorol2 <dorol200@gmail.com>
2020-03-31 00:40:21 +0900

    AOMacro old version

---
+++ MacroExample/MacroExample/Main.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MacroExample
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TestCaptureButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.XPositionTextbox = new System.Windows.Forms.TextBox();
+            this.XPositionLabel = new System.Windows.Forms.Label();
+            this.YPositionLabel = new System.Windows.Forms.Label();
+            this.YPositionTextbox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TestCaptureButton
+            // 
+            this.TestCaptureButton.Location = new System.Drawing.Point(13, 13);
+            this.TestCaptureButton.Name = "TestCaptureButton";
+            this.TestCaptureButton.Size = new System.Drawing.Size(150, 23);
+            this.TestCaptureButton.TabIndex = 0;
+            this.TestCaptureButton.Text = "TestCaptureButton";
+            this.TestCaptureButton.UseVisualStyleBackColor = true;
+            this.TestCaptureButton.Click += new System.EventHandler(this.TestCaptureButtonClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(676, 381);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // XPositionTextbox
+            // 
+            this.XPositionTextbox.Location = new System.Drawing.Point(75, 429);
+            this.XPositionTextbox.Multiline = true;
+            this.XPositionTextbox.Name = "XPositionTextbox";
+            this.XPositionTextbox.Size = new System.Drawing.Size(118, 32);
+            this.XPositionTextbox.TabIndex = 3;
+            this.XPositionTextbox.Text = "0";
+            // 
+            // XPositionLabel
+            // 
+            this.XPositionLabel.AutoSize = true;
+            this.XPositionLabel.Location = new System.Drawing.Point(11, 432);
+            this.XPositionLabel.Name = "XPositionLabel";
+            this.XPositionLabel.Size = new System.Drawing.Size(58, 12);
+            this.XPositionLabel.TabIndex = 2;
+            this.XPositionLabel.Text = "XPosition";
+            this.XPositionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // YPositionLabel
+            // 
+            this.YPositionLabel.AutoSize = true;
+            this.YPositionLabel.Location = new System.Drawing.Point(12, 466);
+            this.YPositionLabel.Name = "YPositionLabel";
+            this.YPositionLabel.Size = new System.Drawing.Size(58, 12);
+            this.YPositionLabel.TabIndex = 5;
+            this.YPositionLabel.Text = "YPosition";
+            this.YPositionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // YPositionTextbox
+            // 
+            this.YPositionTextbox.Location = new System.Drawing.Point(75, 466);
+            this.YPositionTextbox.Multiline = true;
+            this.YPositionTextbox.Name = "YPositionTextbox";
+            this.YPositionTextbox.Size = new System.Drawing.Size(118, 32);
+            this.YPositionTextbox.TabIndex = 4;
+            this.YPositionTextbox.Text = "-15";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(439, 429);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(122, 23);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "StartButton";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(567, 429);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(122, 23);
+            this.StopButton.TabIndex = 7;
+            this.StopButton.Text = "StopButton";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 504);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.YPositionLabel);
+            this.Controls.Add(this.YPositionTextbox);
+            this.Controls.Add(this.XPositionLabel);
+            this.Controls.Add(this.XPositionTextbox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TestCaptureButton);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TestCaptureButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox XPositionTextbox;
+        public System.Windows.Forms.Label XPositionLabel;
+        public System.Windows.Forms.Label YPositionLabel;
+        private System.Windows.Forms.TextBox YPositionTextbox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ MacroExample/MacroExample/Main.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace MacroExample
+{
+    public partial class Main : Form
+    {
+        private const string QT5_WINDOW_ICON = "Qt5QWindowIcon";
+        private const string SCREEN_BOARD_CLASS_WINDOW = "ScreenBoardClassWindow";
+        private const string Q_WIDGET_CLASS_WINDOW = "QWidgetClassWindow";
+
+        [System.Runtime.InteropServices.DllImport("User32", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        internal static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, IntPtr lParam);
+
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr Parent, IntPtr Child, string lpszClass, string lpszWindows);
+
+        public const int WM_LBUTTONDOWN = 0x201;
+        public const int WM_LBUTTONUP = 0x202;
+
+        private const int STAGE_COUNT = 8;
+
+        String AppPlayerName = "NoxPlayer";
+
+        List<Bitmap> stageImages = new List<Bitmap>();
+        List<Bitmap> ingameImages = new List<Bitmap>();
+
+        Bitmap testImage = null;
+        Bitmap autoOffButtonImage= null;
+        Bitmap fightCompleteRainImage = null;
+        Bitmap stageImage = null;
+        Bitmap stageTeamSelectButtonImage = null;
+        Bitmap stageFightButtonImage = null;
+
+        Bitmap stagePageImage = null;
+
+        //앱플레이어 크기를 저정할 변수
+        double full_width = 0;
+        double full_height = 0;
+        //앱플레이어 지정한 크기
+        double pix_width = 615;
+        double pix_height = 375;
+
+        bool roopContinue = false;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            testImage = new Bitmap(@"img\test_img.PNG");
+            autoOffButtonImage = new Bitmap(@"img\auto_button_off_image.PNG");
+            fightCompleteRainImage = new Bitmap(@"img\fightcomplete_rain_image.PNG");
+            stageImage = new Bitmap(@"img\4-4_image.PNG");
+            stageTeamSelectButtonImage = new Bitmap(@"img\stage_team_select_button_image.PNG");
+            stageFightButtonImage = new Bitmap(@"img\stage_fight_button_image.PNG");
+
+            stagePageImage = new Bitmap(@"img\stage_page.PNG");
+
+
+            stageImages.Add(stageImage);
+            stageImages.Add(stageTeamSelectButtonImage);
+            stageImages.Add(stageFightButtonImage);
+
+            //ingameImages.Add(autoOffButtonImage);
+            ingameImages.Add(fightCompleteRainImage);
+            ingameImages.Add(fightCompleteRainImage);
+        }
+
+        private void TestCaptureButtonClick(object sender, EventArgs e)
+        {
+            IntPtr findWindow = FindWindow(null, AppPlayerName);
+            if(findWindow != IntPtr.Zero)
+            {
+                //플레이어를 찾았을 경우
+                Debug.WriteLine("앱플레이어를 찾았습니다.");
+
+                //찾은 플레이어를 바타응로 그래픽스 정보를 가져온다.
+                Graphics Graphicsdata = Graphics.FromHwnd(findWindow);
+
+                //찾은 플레이어 창 크기 및 위치를 가져온다. 
+                Rectangle rect = Rectangle.Round(Graphicsdata.VisibleClipBounds);
+
+                //현재 앱플레이어 화면 크기를 지정해줍니다.
+                full_width = rect.Width;
+                full_height = rect.Height;
+
+                pictureBox1.Width = rect.Width + 50;
+                pictureBox1.Height = rect.Height + 50;
+
+                //플레이어 창 크기만큼의 비트맵을 선언
+                Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+
+                Debug.WriteLine(rect.Width + " / " + rect.Height);
+
+                //비트맵을 바탕으로 그래픽스 함수로 선언
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    //찾은 플레이어의 크기만큼 화면을 캡쳐
+                    IntPtr hdc = g.GetHdc();
+                    PrintWindow(findWindow, hdc, 0x2);
+                    g.ReleaseHdc(hdc);
+                }
+
+                //캡처해온 이미지 사이즈를 변경하여 저장합니다.
+                System.Drawing.Size resize = new System.Drawing.Size((int)pix_width, (int)pix_height);
+                bmp = new Bitmap(bmp, resize);
+
+                pictureBox1.Image = bmp;
+
+                int[] ClcikValue = SearchImagePosition(bmp, testImage);
+                InClick(ClcikValue[0] + (int)Int32.Parse(XPositionTextbox.Text), ClcikValue[1] + (int)Int32.Parse(YPositionTextbox.Text));
+            }
+            else
+            {
+                //플레이어를 못찾은 경우
+                Debug.WriteLine("못찾았어요.");
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            //4-4 -> 팀선택 -> 전투 -> autocheck -> 작전완료 -> 작전완료
+            //autocheck -> 작전완료 -> 작전완료
+            roopContinue = true;
+            for (int i = 0; roopContinue; ++i)
+            {
+                Bitmap screen = CaptureScreen();
+                if (screen != null)
+                {
+                    pictureBox1.Image = screen;
+
+                    int[] clickValue = SearchImagePosition(screen, stageImages[i]);
+                    Debug.WriteLine("stageImages" + i);
+                    if (clickValue[0] > 0 && clickValue[1] > 0)
+                        InClick(clickValue[0] + (int)Int32.Parse(XPositionTextbox.Text), clickValue[1] + (int)Int32.Parse(YPositionTextbox.Text));
+                    else
+                        --i;
+
+                    if (i == 2)
+                    {
+                        //for(int j = 0; j < STAGE_COUNT; ++j)
+                        StartIngameLoop();
+                        i = -1;
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("이미지를 찾을 수 없습니다.");
+                    break;
+                }
+
+                Thread.Sleep(1000);
+            }
+            pictureBox1.Image = null;
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            roopContinue = false;
+        }
+
+        public int[] SearchImagePosition(Bitmap screenImage, Bitmap findImage)
+        {
+            //스크린 이미지 선언
+            using (Mat ScreenMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(screenImage))
+            // 찾을 이미지 선언
+            using (Mat FindMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(findImage))
+            //스크린 이미지에서 FindMat 이미지를 찾아라
+            using (Mat res = ScreenMat.MatchTemplate(FindMat, TemplateMatchModes.CCoeffNormed))
+            {
+                //찾은 이미지의 유사도를 담은 더블형 최대 최소 값을 선언
+                double minval, maxval = 0;
+
+                //찾은 이미지의 위치를 담은 포인트형을 선언한다.
+                OpenCvSharp.Point minloc, maxloc;
+
+                //찾은 이미지의 유사도 및 위치 값을 받는다. 
+                Cv2.MinMaxLoc(res, out minval, out maxval, out minloc, out maxloc);
+                Debug.WriteLine($"찾은 이미지의 유사도 : {maxval}, 위치 : {minloc}, {maxloc}");
+
+                double change_size = full_width / pix_width;
+
+                int[] returnValue = new int[2];
+                if (maxval >= 0.7)
+                {
+                    returnValue[0] = maxloc.X + findImage.Width/2;
+                    returnValue[1] = maxloc.Y + findImage.Height/2;
+                }
+
+                return returnValue;
+            }
+        }
+
+        public void InClick(int x, int y)
+        {
+            //클릭이벤트를 발생시킬 플레이어를 찾습니다.
+            IntPtr findwindow = FindWindow(null, AppPlayerName);
+            if (findwindow != IntPtr.Zero)
+            {
+                //플레이어를 찾았을 경우 클릭이벤트를 발생시킬 핸들을 가져옵니다.
+                IntPtr hwnd_child = FindWindowEx(findwindow, IntPtr.Zero, QT5_WINDOW_ICON, SCREEN_BOARD_CLASS_WINDOW);
+                IntPtr lparam = new IntPtr(x | (y << 16));
+
+                //플레이어 핸들에 클릭 이벤트를 전달합니다.
+                SendMessage(hwnd_child, WM_LBUTTONDOWN, 1, lparam);
+                SendMessage(hwnd_child, WM_LBUTTONUP, 0, lparam);
+            }
+        }
+
+        private Bitmap CaptureScreen()
+        {
+            IntPtr findWindow = FindWindow(null, AppPlayerName);
+            if (findWindow != IntPtr.Zero)
+            {
+                //플레이어를 찾았을 경우
+                Debug.WriteLine("앱플레이어를 찾았습니다.");
+
+                //찾은 플레이어를 바탕으로 그래픽스 정보를 가져온다.
+                Graphics Graphicsdata = Graphics.FromHwnd(findWindow);
+
+                //찾은 플레이어 창 크기 및 위치를 가져온다. 
+                Rectangle rect = Rectangle.Round(Graphicsdata.VisibleClipBounds);
+
+                //현재 앱플레이어 화면 크기를 지정해줍니다.
+                full_width = rect.Width;
+                full_height = rect.Height;
+
+                pictureBox1.Width = rect.Width + 50;
+                pictureBox1.Height = rect.Height + 50;
+
+                //플레이어 창 크기만큼의 비트맵을 선언
+                Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+
+                //비트맵을 바탕으로 그래픽스 함수로 선언
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    //찾은 플레이어의 크기만큼 화면을 캡쳐
+                    IntPtr hdc = g.GetHdc();
+                    PrintWindow(findWindow, hdc, 0x2);
+                    g.ReleaseHdc(hdc);
+                }
+
+                //캡처해온 이미지 사이즈를 변경하여 저장합니다.
+                System.Drawing.Size resize = new System.Drawing.Size((int)pix_width, (int)pix_height);
+                bmp = new Bitmap(bmp, resize);
+
+                return bmp;
+            }
+            //플레이어를 못찾은 경우
+            Debug.WriteLine("못찾았어요.");
+            return null;
+        }
+
+        private void StartIngameLoop()
+        {
+            if (CheckStagePage())
+                return;
+
+            Debug.WriteLine("StartIngameLoop");
+            for (int i = 0; ingameImages.Count > i; ++i)
+            {
+                Bitmap screen = CaptureScreen();
+                if (screen != null)
+                {
+                    if (CheckStagePage())
+                        return;
+
+                    pictureBox1.Image = screen;
+
+                    int[] clickValue = SearchImagePosition(screen, ingameImages[i]);
+                    //Debug.WriteLine("ingameImages" + i);
+                    if (clickValue[0] > 0 && clickValue[1] > 0)
+                        InClick(clickValue[0] + (int)Int32.Parse(XPositionTextbox.Text), clickValue[1] + (int)Int32.Parse(YPositionTextbox.Text));
+                    else
+                        --i;
+                }
+                else
+                {
+                    Debug.WriteLine("이미지를 찾을 수 없습니다.");
+                    break;
+                }
+
+                Thread.Sleep(1000);
+            }
+            if (!CheckStagePage())
+                StartIngameLoop();
+        }
+
+        private bool CheckStagePage()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("CheckStagePage");
+            Bitmap screen = CaptureScreen();
+            if (screen != null)
+            {
+                pictureBox1.Image = screen;
+
+                int[] clickValue = SearchImagePosition(screen, stagePageImage);
+                if (clickValue[0] > 0 && clickValue[1] > 0)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
